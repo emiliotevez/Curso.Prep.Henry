1f595969c0a8abec167ef503fe5ec0986fd4b71b
Emilio <tevezemilio@gmail.com>
2022-02-20 20:04:35 -0300

    3 commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -28,14 +28,14 @@ const nuevoModulo = 21 % 5 === 1 ;
 function devolverString(str) {
   // "Regresar" la cadena provista: nombre
   // Tu codigo:
-  devolverString (str)
+  return devolverStringstr
 }
 
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  suma (x + y);
+  return sumax + y;
 }
 
 function resta(x, y) {
@@ -60,7 +60,7 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (x === y) {
+  if ( x === y ) {
     return true;
   } {
     return false;
@@ -71,7 +71,7 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1 === str2) {
+  if ( str1 === str2 ) {
     return true;
   } {
     return false;
@@ -82,7 +82,7 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num < 90) { 
+  if ( num < 90 ) {
     return true;
   } {
     return false;
